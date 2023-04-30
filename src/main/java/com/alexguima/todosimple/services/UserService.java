1a1ce64ef5f8993ae94990e92d62a42d6ae1b7fe
Alex-Guima <alexandreagtaveira@gmail.com>
2023-04-30 12:31:31 -0300

    Create TaskService and update UserService

--- src/main/java/com/alexguima/todosimple/services/UserService.java
+++ src/main/java/com/alexguima/todosimple/services/UserService.java
@@ -1,7 +1,6 @@
 package com.alexguima.todosimple.services;
 
 import com.alexguima.todosimple.models.User;
-import com.alexguima.todosimple.repositories.TaskRepository;
 import com.alexguima.todosimple.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,8 +15,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
 
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
@@ -30,7 +27,6 @@ public class UserService {
     public User create(User obj) {
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
